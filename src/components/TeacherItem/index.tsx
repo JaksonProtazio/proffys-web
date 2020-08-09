40cc0ab1dd169a0a80548ad76352f2a87005e6b2
Jakson <jakson.protazio@oceanbrasil.com>
2020-08-09 13:29:35 -0400

    Página 'Dar aula' finalizada e frontend linkado ao backend

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,33 +3,50 @@ import React from 'react'
 import whatsappIcon from "../../assets/images/icons/whatsapp.svg"
 
 import "./styles.css" 
+import api from '../../services/api'
 
-function TeacherItem(){
+export interface Teacher{
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
+    function createNewConnection(){
+        api.post('connections',{
+            user_id: teacher.id
+        });
+    }
+
     return(
         <article className="teacher-item">
             <header>
-                <img src="https://avatars1.githubusercontent.com/u/35311748?s=460&u=06caf6fdc4ff80a07ad5a38a3fa681c8f80de6e0&v=4" alt="Jakson Protázio"/> 
+                <img src={teacher.avatar} alt={teacher.name}/> 
                 <div>
-                    <strong>Jakson Protázio</strong>
-                    <span>Html</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                <br/> <br/>
-                Fuga sunt placeat quod quaerat molestias dignissimos vero, incidunt veritatis quas asperiores praesentium ad, excepturi veniam id maxime suscipit earum odio perferendis.
-            </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                    <strong>R$ 60,00</strong>
+                    <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a target="_blank" onClick={createNewConnection} href={`https://wa.me/55${teacher.whatsapp}`}>
                     <img src={whatsappIcon} alt="Whatsapp"/>
                     Entrar em contato
-                </button>
+                </a>
             </footer>
         </article>
     )
